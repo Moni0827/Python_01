84e29b61e78fb286ecfdec03b238ee2ca155ca01
Moni0827 <monica08270326@gmail.com>
2025-02-18 21:37:55 +0800

    新增 class.py 檔案，示範變數使用格式化字串

---
+++ Class3/class3.py
@@ -0,0 +1,16 @@
+# 變數
+# 變數可以使用=將右邊的資料存到左邊的變數中
+# 變數名稱不能有空白，不能有符號，不能是數字，但可以用_
+# 變數名稱有大小寫區分
+# myName, my_name
+a = True  # 布林值
+print(a)
+
+myName = 123
+print(myName)
+
+name = "St.Monica"
+age = 1694
+print("hello,{name},you are {age} years old")
+# f-string(format string)
+print(f"hello,{name},your age is {age}")
